d0257a8b08b5d82437e57c680156751bbf0eac8a
Annastasia Psitos <annastasia.psitos@gmail.com>
2020-04-06 22:05:19 -0600

    Start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const initialList = [{title: 'The Far Pavilions', author: "M.M. Kaye"}, {title: "Justinian's Flea", author: ''}, {title: 'Victorians Undone', author: ''} ]
+  const [list, setAddToList] = useState(initialList);
+  const [title, setTitle] = useState();
+  const [author, setAuthor] = useState()
+
+  const showList = () => {
+   return list.map(book => {
+     return (
+     <li>{book.title} by {book.author}</li>)
+    })
+  }
+
+  const addToList = () => {
+    console.log('WHAT IS THE LIST', list)
+    const newBook = list.push({title: title, author: author})
+    console.log('WHAT IS NEW LIST', newBook)
+    // setAddToList(newBook)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="booklist">
+      <header>My List of Books to Read</header>
+      <ul>
+      {showList()}
+      </ul>
+      <form>
+        <input placeholder="title" value={title} onChange={() => setTitle(title)} />
+        <input placeholder="author" value={author} onChange={() => setAuthor(author)} />
+      </form>
+      <button onClick={() => {addToList()}}>Add to List!</button>
     </div>
   );
 }
